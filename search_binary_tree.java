ad57c54f8ab9571207ca6dd312d6963a31e892c2
Zisan-uzum <uzummzisann@gmail.com>
2020-12-16 20:13:03 +0300

    leetcode search in binary search tree

---
+++ search_binary_tree.java
@@ -0,0 +1,33 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class search_binary_tree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode ans;
+        if (root == null || root.val == val) {
+            ans = root;
+        } else if (val > root.val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+
+        return ans;
+    }
+}
